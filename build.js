caca17d29a3b772ff659058dcbef2b1a95c8e267
Alex Mueller <amueller474@gmail.com>
2026-05-11 15:08:52 -0400

    Add youtube link to markdown to test embed rendering

--- build.js
+++ build.js
@@ -23,7 +23,6 @@ function replaceYouTubeLinks(markdown) {
     height="315"
     src="https://www.youtube.com/embed/${videoId}"
     title="YouTube video player"
-    frameborder="0"
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
     allowfullscreen>
   </iframe>
--- src/content/works.md
+++ src/content/works.md
@@ -1,3 +1,4 @@
 # Mirrors
 ## for flute and piano (2021)
 Commissioned by Jill Heyboer. Premiered 2006 in Springfield, MO.
+https://www.youtube.com/watch?v=Uh-12fPIGgE
